dc8befa6772b55d0f73b5a13e561f0171eaf7986
Davinellulinvega <casparluc@gmail.com>
2015-12-01 11:20:37 +0000

    Commented the sphero command out for debugging of the max_length computation

--- mouseControl.py
+++ mouseControl.py
@@ -6,6 +6,7 @@ import pyautogui
 from sphero_driver import sphero_driver
 from math import atan2
 from math import cos
+from math import sin
 from math import sqrt
 from math import pi
 
@@ -27,18 +28,19 @@ if x == 0:
 # Create a sphero object
 sphero = sphero_driver.Sphero(target_addr="68:86:E7:06:30:CB")
 # Connect to the robot
-sphero.connect()
+#sphero.connect()
+sphero.is_connected = True
 
 # If we were able to connect to the robot
 if sphero.is_connected:
     # Set the heading to 0
-    sphero.set_heading(0x00, False)
+    #sphero.set_heading(0x00, False)
     # Set the stabilization
-    sphero.set_stablization(True, False)
+    #sphero.set_stablization(True, False)
     # Set the heading to 0 for the moment
-    sphero.roll(0x00, 0x00, 0x00, False)
+    #sphero.roll(0x00, 0x00, 0x00, False)
     # Turn the tail light on
-    sphero.set_back_led(0x255, False)
+    #sphero.set_back_led(0x255, False)
 
     # Initialize the variables
     speed = 0
@@ -59,20 +61,19 @@ if sphero.is_connected:
             # Compute the maximum length reachable by the mouse
             if 0 <= heading_rad <= crit_angle:
                 # Use the half_w
-                adj = half_w
-            elif heading_rad < (pi - heading_rad):
+                max_length = half_w / cos(heading_rad)
+            elif heading_rad < (pi - crit_angle):
                 # Use the half_h
-                adj = half_h
-            elif heading_rad <= (pi + heading_rad):
+                max_length = half_h / sin(heading_rad)
+            elif heading_rad <= (pi + crit_angle):
                 # Use the half_w
-                adj = half_w
-            elif heading_rad < (2*pi - heading_rad):
+                max_length = half_w / cos(heading_rad)
+            elif heading_rad < (2*pi - crit_angle):
                 # Use the half_h
-                adj = half_h
+                max_length = half_h / sin(heading_rad)
             else:
                 # Use the half_w
-                adj = half_w
-            max_length = adj / cos(heading_rad)
+                max_length = half_w / cos(heading_rad)
 
             # Compute the speed. 255 being the max speed for the robot.
             length = sqrt((x**2 + y**2))
@@ -83,7 +84,8 @@ if sphero.is_connected:
                 speed = 255
 
             # Ask the robot to roll
-            sphero.roll(int(speed), int(heading), 0x01, False)
+            print(speed, heading)
+    #        sphero.roll(int(speed), int(heading), 0x01, False)
 
             # Get the new mouse position
             x, y = pyautogui.position()
@@ -98,10 +100,10 @@ if sphero.is_connected:
 
     except KeyboardInterrupt:
         # Ask the robot to stop rolling
-        sphero.roll(0x00, 0x00, 0x00, False)
+    #    sphero.roll(0x00, 0x00, 0x00, False)
         # Turn the tail light off
-        sphero.set_back_led(0x00, False)
+    #    sphero.set_back_led(0x00, False)
         # Disconnect from the robot
-        sphero.disconnect()
+    #    sphero.disconnect()
         # Print a little message
         print("Goodbye all you people !!!")
